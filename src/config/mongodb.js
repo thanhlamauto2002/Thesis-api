a3b54b460722b34671c3893d5007cc5f935f9d9b
thanhlam <thanhlama102@gmail.com>
2024-03-12 22:35:41 +0700

    connect_mongodb_env

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,31 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+
+
+
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
+
+let doanDatabaseInstance = null
+
+//Khoi tao 1 doi tuong mongoClientInstance de connect toi MongoDB
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+
+})
+export const CONNECT_DB = async () => {
+  //Goi ket noi toi MongoDB Atlas voi URI da khai bao trong than cua clientInstance
+  await mongoClientInstance.connect()
+  doanDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+
+}
+export const GET_DB = () => {
+  if (!doanDatabaseInstance) throw Error('Must connect to Database first!')
+  return doanDatabaseInstance
+}
